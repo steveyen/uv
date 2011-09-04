000e66f5298e4dc1b3d59ae3e1902bf06b83d1e5
Steve Yen <steve.yen@gmail.com>
2011-09-03 18:30:12 -0700

    More tests

--- test.lua
+++ test.lua
@@ -17,4 +17,40 @@ assert(uv_wrap.UV_UNKNOWN == -1)
 assert(uv_wrap.UV_OK == 0)
 assert(uv_wrap.UV_EOF == 1)
 
-print("OK")
+print("OK - basic enums")
+
+uv_wrap.uv_init()
+
+d0 = uv_wrap.uv_default_loop()
+assert(d0)
+
+d1 = uv_wrap.uv_default_loop()
+assert(d1)
+assert(d1 ~= d0)
+
+t0 = uv_wrap.uv_now(d0)
+assert(t0 > 0)
+uv_wrap.uv_update_time(d0)
+t1 = uv_wrap.uv_now(d0)
+assert(t1 >= t0)
+
+e0 = uv_wrap.uv_last_error(d0)
+assert(uv_wrap.uv_err_name(e0) == "OK")
+
+b0 = uv_wrap.uv_buf_init("hello", 5)
+assert(b0)
+
+print("OK - simple tests")
+
+stdin = uv_wrap.uv_std_handle(d0, uv_wrap.UV_STDIN)
+assert(stdin)
+
+stdout = uv_wrap.uv_std_handle(d0, uv_wrap.UV_STDOUT)
+assert(stdout)
+
+stderr = uv_wrap.uv_std_handle(d0, uv_wrap.UV_STDERR)
+assert(stderr)
+
+print("OK - stdin/out/err available")
+
+print("OK - done")
